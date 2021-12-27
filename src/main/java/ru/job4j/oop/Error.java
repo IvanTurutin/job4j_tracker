9fe5c1f3b1cd38556c05941df36a296783622113
Ivan <ivan.turutin@gmail.com>
2021-12-27 20:02:49 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,33 @@
+package ru.job4j.oop;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error (boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public Error () {
+    }
+
+    public void printInfo () {
+        System.out.println(active);
+        System.out.println(status);
+        System.out.println(message);
+    }
+
+    public static void main(String[] args) {
+        Error errorDefault = new Error();
+        Error error1 = new Error(true, 3, "Статус 3");
+        Error error2 = new Error(false, 2, "Статус 2");
+        errorDefault.printInfo();
+        error1.printInfo();
+        error2.printInfo();
+
+    }
+
+}
