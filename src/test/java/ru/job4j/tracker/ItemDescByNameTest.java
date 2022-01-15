45ddeba47a4e2f62d06b06ae2ee42f06925e77d6
Ivan <ivan.turutin@gmail.com>
2022-01-15 22:25:17 +0300

    0. Сортировка [#10096]

---
+++ src/test/java/ru/job4j/tracker/ItemDescByNameTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class ItemDescByNameTest {
+
+    @Test
+    public void whenDescSort() {
+        List<Item> items = new ArrayList<>();
+        Item item1 = new Item(1, "Book");
+        Item item2 = new Item(2, "Pen");
+        Item item3 = new Item(3, "Notebook");
+        items.add(item1);
+        items.add(item2);
+        items.add(item3);
+        Collections.sort(items, new ItemDescByName());
+        List<Item> expected = new ArrayList<>();
+        expected.add(item2);
+        expected.add(item3);
+        expected.add(item1);
+        assertThat(expected, is(items));
+    }
+}
