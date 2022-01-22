4fee1e0499e80213d1ceca57bd3459addbef9fdc
Ivan <ivan.turutin@gmail.com>
2022-01-22 19:13:02 +0300

    5. Подсчет функции в диапазоне. [#24251]

--- src/main/java/ru/job4j/lambda/Functions.java
+++ src/main/java/ru/job4j/lambda/Functions.java
@@ -7,7 +7,7 @@ import java.util.function.Function;
 
 public class Functions {
 
-    public static List<Double> diapason(int start, int end, Function<Double, Double> func){
+    public static List<Double> diapason(int start, int end, Function<Double, Double> func) {
         List<Double> diap = new ArrayList<>();
         for (int i = start; i < end; i++) {
             Double num = Double.valueOf(i);
